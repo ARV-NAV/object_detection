25701da81c58e424700cf83ed48957c216429483
Jasmine Nahorniak <jasmine@coas.oregonstate.edu>
2020-03-09 21:37:12 +0000

    Added workaround to get either camera 1 or camera 0 if 1 not present

--- capture.py
+++ capture.py
@@ -4,9 +4,16 @@ import cv2 as cv
 import sys
 import os.path
 
-camera1 = int(sys.argv[1])
+#camera1 = int(sys.argv[1])
 
-cap1 = cv.VideoCapture(camera1)
+# Hacky workaround to make sure we get SOME video... Hopefully it's the right video!
+camera1 = 1
+cap1 = cv.VideoCapture(camera1, cv.CAP_DSHOW)
+if cap1 is None or not cap1.isOpened():
+    print("camera 1 not detected! defaulting to camera 0")
+    camera1 = 0
+
+cap1 = cv.VideoCapture(camera1, cv.CAP_DSHOW)
 
 fourcc = cv.VideoWriter_fourcc(*'XVID')
 
@@ -20,12 +27,13 @@ out1 = None
 print("Press \'q\' to stop recording!")
 while True:
     # Use grab and retrieve to have cameras closer to being synced timewise
-    cap1.grab()
-    ret1, frame1 = cap1.retrieve()
+    #cap1.grab()
+    #ret1, frame1 = cap1.retrieve()
+    ret1, frame1 = cap1.read()
     if out1 == None:
         (w, h) = frame1.shape[:2]
         print("Recording video at resolution " + str(w) + "x" + str(h))
-        out1 = cv.VideoWriter(filename1,fourcc, 20.0, (w, h))
+        out1 = cv.VideoWriter(filename1,fourcc, 20.0, (h, w))
 		
 	
     if ret1 == True:
